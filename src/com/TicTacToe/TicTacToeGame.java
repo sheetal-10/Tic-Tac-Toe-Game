42b926fdcc146a8faf01e5d11b40923471bcd476
sheetal-10 <sheetalp257@gmail.com>
2021-08-10 23:22:31 +0530

    Added: method for user to choose mark

--- src/com/TicTacToe/TicTacToeGame.java
+++ src/com/TicTacToe/TicTacToeGame.java
@@ -1,5 +1,7 @@
 package com.TicTacToe;
 
+import java.util.Scanner;
+
 /**
  * Program for TicTacToe Game playing with computer 
  * @author Sheetal
@@ -8,12 +10,15 @@ package com.TicTacToe;
 public class TicTacToeGame {
 	
 	static char[] board = new char[10];
+	static char userMark, computerMark;
 	
 	public static void main(String[] args) {
 		//displaying welcome message
 		System.out.println("Welcome to Tic Tac Toe Game");
 		//calling method to initialise the board
 		createBoard();
+		//for user to choose 'X' or 'O' mark
+		choosingXorO();
 	}
 	
 	/**
@@ -21,10 +26,32 @@ public class TicTacToeGame {
 	 * 0th index is ignored
 	 */
 	private static void createBoard() {
-		
 		for (int i = 1; i < 10; i++) {
 			board[i] = ' ';
 		}
 	}
+	
+	/**
+	 * Asking user to choose X or O
+	 * @param option
+	 * @return userMark, computerMark
+	 */
+	private static void choosingXorO() {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Choose 1 for 'X' or Choose 2 for 'O' as your mark");		
+		int option = s.nextInt();
+		switch (option) {
+			case 1: userMark = 'X';
+					computerMark = 'O';
+					break;
+			case 2: userMark = 'O';
+					computerMark = 'X';
+					break;
+			default:
+					System.out.println("Your input is invalid");
+					choosingXorO();
+		}
+	}
 }
+
 
