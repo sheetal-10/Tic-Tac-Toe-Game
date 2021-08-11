f0c545826242e22ef1acf17588f57b44a9a2183b
sheetal-10 <sheetalp257@gmail.com>
2021-08-12 00:03:17 +0530

    Added: method for computer to choose side and then corner

--- src/com/TicTacToe/TicTacToeGame.java
+++ src/com/TicTacToe/TicTacToeGame.java
@@ -65,6 +65,12 @@ public class TicTacToeGame {
 						flag=0;
 						return;
 					}
+					flag=computerCenterSide();
+					if (flag==1) {
+						turn++;
+						flag=0;
+						return;
+					}
 				}
 			}
 		}
@@ -283,6 +289,29 @@ public class TicTacToeGame {
 				System.out.println("Cmputer choice is '"+corner[i]+"'");
 				flag=1;
 				break;
+			}
+		}
+		return flag;
+	}
+	
+	/**
+	 * Making computer to choose center and then side
+	 * @return flag 
+	 */
+	private static int computerCenterSide() {
+		if (element[5] != 'X' && element[5] != 'O') {
+			element[5]=computerMark;
+			System.out.println("Computer choice is '5'");
+			flag=1;
+		} else {
+			int side[] = {2,6,8,4};
+			for(int j=0;j<4;j++) {
+				if(element[side[j]] != 'X' && element[side[j]] != 'O') {
+					element[side[j]]=computerMark;
+					System.out.println("My choice is '"+side[j]+"'");
+					flag=1;
+					break;
+				}
 			}
 		}
 		return flag;
